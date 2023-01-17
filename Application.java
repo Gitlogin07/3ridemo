4d5b55b3fcd557bc157c0e8aa6a8ae8e008df8a4
Harshada <122136165+Gitlogin07@users.noreply.github.com>
2023-01-17 14:23:19 +0530

    Add files via upload

---
+++ Application.java
@@ -0,0 +1,18 @@
+package com.project.ri.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@SpringBootApplication
+public class Application {
+	
+	public static void main(String[] args) {
+
+		System.out.println("Hello");
+		SpringApplication.run(Application.class, args);
+	}
+
+}
